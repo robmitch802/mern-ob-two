8d046717db82404b3b9c566bbbb7f541b3ddc433
robmitch802 <60521912+robmitch802@users.noreply.github.com>
2020-11-05 10:27:49 -0500

    update delete fxn

--- backend/routes/articles.js
+++ backend/routes/articles.js
@@ -23,8 +23,34 @@ router.route('/add').post((req, res) => {
         .catch(err => res.status(400).json('Error in post request: ' + err));
 });
 
-router.route('/update/:id').post((req, res) => {
-    
+router.route('/:id').get((req, res) => {
+    Article.findById(req.params.id)
+        .then(article => res.json(article))
+        .catch(err => res.status(400).json('Error in get request: ' + err));
 })
+
+router.route('/:id').delete((req, res) => {
+    Article.findByIdAndDelete(req.params.id)
+        .then(() => res.json('Article deleted!'))
+        .catch(err => res.status(400).json('Error in delete request: ' + err));
+})
+
+router.route('/update/:id').post((req, res) => {
+    Article.findById(req.params.id)
+        .then(article => {
+            article.headline = req.body.headline;
+            article.excerpt = req.body.excerpt;
+            article.content = req.body.content;
+            article.date = Date.parse(req.body.date);
+            article.user = req.body.user;
+            //article.image = req.body.image;
+            //article.caption = req.body.caption;
+
+            article.save()
+                .then(() => res.json('Article updated!'))
+                .catch(err => res.status(400).json('Error in update request: ' + err));
+        })
+        .catch(err => res.status(400).json('Error in delete request: ' + err));
+});
 
 module.exports = router;
--- backend/routes/meetings.js
+++ backend/routes/meetings.js
@@ -25,4 +25,34 @@ router.route('/add').post((req, res) => {
         .catch(err => res.status(400).json('Error in post request: ' + err));
 });
 
+router.route('/:id').get((req, res) => {
+    Meeting.findById(req.params.id)
+        .then(meeting => res.json(meeting))
+        .catch(err => res.status(400).json('Error in get request: ' + err));
+})
+
+router.route('/:id').delete((req, res) => {
+    Meeting.findByIdAndDelete(req.params.id)
+        .then(() => res.json('Meeting deleted!'))
+        .catch(err => res.status(400).json('Error in delete request: ' + err));
+})
+
+router.route('/update/:id').post((req, res) => {
+    Meeting.findById(req.params.id)
+        .then(meeting => {
+            meeting.headline = req.body.headline;
+            meeting.excerpt = req.body.excerpt;
+            meeting.content = req.body.content;
+            meeting.date = Date.parse(req.body.date);
+            meeting.user = req.body.user;
+            //meeting.image = req.body.image;
+            //meeting.caption = req.body.caption;
+
+            meeting.save()
+                .then(() => res.json('Meeting updated!'))
+                .catch(err => res.status(400).json('Error in update request: ' + err));
+        })
+        .catch(err => res.status(400).json('Error in delete request: ' + err));
+});
+
 module.exports= router;
